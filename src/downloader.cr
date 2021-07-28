2c75466deaa5fc2dff4bc370ea484ae18813c1e0
trollmao <trollmao@protonmail.com>
2021-07-28 16:18:59 -0500

    shard complete

--- src/downloader.cr
+++ src/downloader.cr
@@ -1,6 +1,14 @@
 # TODO: Write documentation for `Downloader`
+require "http/client"
+
 module Downloader
   VERSION = "0.1.0"
 
-  # TODO: Put your code here
+  def self.download(@@url : String, @@filename : String)
+    HTTP::Client.get(url) do |response|
+      if response.status_code == 200
+        File.write(filename, response.body_io)
+      end
+    end
+  end
 end
